26b95b90a7886b603fc697cf03bb55e2e1b7fc83
prabinrijal03 <prabinrijal03@gmail.com>
2024-10-21 11:12:29 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_app/models/task.dart';
+import 'package:flutter_bloc_app/screens/task_screen.dart';
+
+import 'blocs/bloc_exports.dart';
+
+void main() {
+  // ignore: deprecated_member_use
+  BlocOverrides.runZoned(
+    () => runApp(
+      const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => TasksBloc()
+        ..add(AddTask(
+          task: Task(title: 'Task1'),
+        )),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const TaskScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/task_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import '../blocs/bloc_exports.dart';
+import '../models/task.dart';
+import '../widgets/tasks_list.dart';
+import 'add_task_screen.dart';
+
+class TaskScreen extends StatefulWidget {
+  const TaskScreen({super.key});
+
+  @override
+  State<TaskScreen> createState() => _TaskScreenState();
+}
+
+class _TaskScreenState extends State<TaskScreen> {
+  void _addTask(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) => SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom),
+                child: const AddTaskScreen(),
+              ),
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TasksBloc, TasksState>(
+      builder: (context, state) {
+        List<Task> tasksList = state.allTasks;
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Task App'),
+            actions: [
+              IconButton(
+                onPressed: () => _addTask(context),
+                icon: const Icon(Icons.add),
+              ),
+            ],
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Center(
+                child: Chip(
+                  label: Text('Tasks:'),
+                ),
+              ),
+              TasksList(tasksList: tasksList)
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => _addTask(context),
+            tooltip: 'Add Task',
+            child: const Icon(Icons.add),
+          ),
+        );
+      },
+    );
+  }
+}
